0df7546bc8f204dca5485b78e4548ec9c3b1ab2f
Insertskinhere <31601072+adam2006@users.noreply.github.com>
2019-09-21 23:36:38 +0200

    add learning game

---
+++ Learning/Assets/Respawn.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour {
+
+	[SerializeField] private Transform player;
+	[SerializeField] private Transform respawnPoint;
+	public GameObject text;
+
+	void OnTriggerEnter(Collider col){
+		player.transform.position = respawnPoint.transform.position;
+		text.SetActive(true);
+	}
+
+
+}
---
+++ Learning/Assets/TextTriger1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextTriger1 : MonoBehaviour {
+
+
+	public GameObject text;
+
+	void OnTriggerEnter(Collider col)
+	{
+		
+			text.SetActive(true);
+	}
+			
+			
+			
+}
+
